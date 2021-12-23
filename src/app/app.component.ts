1f6f688ac9a58ba9e58a60cc04cf532d78cdd203
andresp406 <oracle406>
2021-12-23 08:11:47 -0500

    configurando proyecto e instalando paquetes necesarios

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { ToastrService } from 'ngx-toastr';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,14 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'placetopay-front';
+
+  constructor(private _toast:ToastrService){
+
+  }
+
+  showSuccess() {
+    this._toast.success('Hello world!', 'Toastr fun!');
+  }
+
+
 }
