91505a475e90a809a7a04d7c302c24898f062c2b
andresp406 <oracle406>
2021-12-24 17:38:37 -0500

    asignandole guard a la ruta de login y registro

---
+++ src/app/guard/auth.guard.ts
@@ -0,0 +1,32 @@
+import { Injectable } from '@angular/core';
+import { ActivatedRouteSnapshot, CanActivate, Router, RouterStateSnapshot, UrlTree } from '@angular/router';
+import { Observable } from 'rxjs';
+import { AuthService } from '../services/auth.service';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class AuthGuard implements CanActivate {
+ 
+  constructor(
+    private _authService: AuthService,
+    private _router: Router) { }
+
+  canActivate(
+    route: ActivatedRouteSnapshot,
+    state: RouterStateSnapshot): Observable<boolean | UrlTree> | Promise<boolean | UrlTree> | boolean | UrlTree {
+
+    return new Promise((resp, reject) => {
+      this._authService.check()
+        .subscribe((response: any) => {
+          //console.log(response)
+          resp(true)
+        }, err => {
+          console.log(err);
+          resp(false);
+         // this._authService.removeCurrentUser()
+          this._router.navigate(['/']);
+        })
+    })
+  }
+}
--- src/app/services/auth.service.ts
+++ src/app/services/auth.service.ts
@@ -1,9 +1,38 @@
 import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 
 @Injectable({
   providedIn: 'root'
 })
 export class AuthService {
 
-  constructor() { }
+  constructor(
+    private _http:HttpClient
+  ) { }
+
+  get getToken(): string {
+    return localStorage.getItem('x-token') || '';
+  }
+  
+  check():Observable<any> {
+    const headers = this._getHeaders();
+    const url = ``;
+    return this._http.get<any>(url, {headers});
+  }
+
+  private _getHeaders(): HttpHeaders {
+    const token = this.getToken
+
+    if (token.length < 10) {
+     // this.removeCurrentUser();
+      return new HttpHeaders();
+    }
+
+    const headers = new HttpHeaders({
+      Authorization: `Bearer ${token}`,
+    })
+
+    return headers;
+  }
 }
